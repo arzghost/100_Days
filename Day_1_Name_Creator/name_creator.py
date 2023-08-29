ac38e9bb3684f52fa452371a5be2a32bf0897886
arzghost <arzflint@gmail.com>
2023-08-30 00:37:57 +0300

    Day 1 - 0.3

---
+++ Day_1_Name_Creator/name_creator.py
@@ -0,0 +1,13 @@
+﻿from functools import reduce
+import random
+import os
+
+path = os.path.dirname(__file__)
+
+def choose_from_file(filename):
+    with open(os.path.join(path, filename), encoding='utf-8-sig') as inf:
+        return random.choice(list(reduce(lambda x, y: x + y, [line.strip().split('\t') for line in inf.readlines()], [])))
+
+animal, action, obj = map(choose_from_file, ('animals.txt', 'actions.txt', 'object.txt'))
+
+print(animal, action, obj)
