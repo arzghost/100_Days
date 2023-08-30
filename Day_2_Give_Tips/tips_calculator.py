89b8e5ae78215db4e9a6914d35a7ada7826ad182
arzghost <arzflint@gmail.com>
2023-08-30 21:35:45 +0300

    day 2 - 1.0

---
+++ Day_2_Give_Tips/tips_calculator.py
@@ -0,0 +1,32 @@
+﻿bill, percentage, people_number = '', '', ''
+
+while bill == '':
+    try:
+        enter = input('Enter your total bill: $')
+        bill = float(enter)
+    except ValueError:
+        print(f'Your value {enter} is incorrect')
+        bill = ''
+
+while percentage == '':
+    try:
+        enter = input('Enter your percent for tips: ')
+        percentage = float(enter)
+    except ValueError:
+        print(f'Your value {enter} is incorrect')
+        percentage = ''
+
+while people_number == '':
+    try:
+        enter = input('Enter number of people: ')
+        people_number = int(enter)
+        1 / people_number
+    except ValueError:
+        print(f'Your value {enter} is incorrect')
+        people_number = ''
+    except ZeroDivisionError:
+        print(f'Number of people cannot equal to 0.')
+        people_number = ''
+
+
+print(f'Everyone pays ${round(bill * percentage / (100 * people_number), 2)}')
