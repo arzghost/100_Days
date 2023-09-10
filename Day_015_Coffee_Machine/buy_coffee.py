45939905647d40063067730457928da7ab2ece1b
arzghost <arzflint@gmail.com>
2023-09-10 16:57:45 +0300

    day 15 - 0.1

---
+++ Day_015_Coffee_Machine/buy_coffee.py
@@ -0,0 +1,54 @@
+﻿from coffeeMachine import *
+from os import system
+from time import sleep
+
+
+def get_money(coins):
+    total = 0
+    for key in coins.keys():
+        total += int(input(f'How many {key} will you put? '))* coins[key]
+    return total
+
+if __name__ == '__main__':
+    commands = ['espresso', 'americano', 'capucchino', 'latte', 'status', 'add', 'stop', 'break', 'halt', 'take']
+    while True:
+        sleep(2)
+        system('cls')
+        request = input('''Which coffee would you like? (type espresso/americano/capucchino/latte): ''').lower()
+        if request not in commands:
+            print('Wrong command!')
+        elif request == 'stop' or request == 'break' or request =='halt':
+            print('Machine is stopped.')
+            break
+        elif request == 'status':
+            for key, value in machine.items():
+                print(f'{key.capitalize()}: {value}.')
+            input('Press Enter to continue...')
+        elif request == 'add':
+            machine['water'] += int(input('How many milliliters of water would you like to add?: '))
+            machine['coffee'] += int(input('How many grams of coffee would you like to add?: '))
+            machine['milk'] += int(input('How many milliliters of milk would you like to add?: '))
+            print('Everything was add in coffee machine!')
+        elif request == 'take':
+            machine['money'] = 0
+            print('Money were taken! :)')
+        else:
+            if recipes[request]['water'] > machine['water'] or recipes[request]['coffee'] > machine['coffee'] or recipes[request]['milk'] > machine['milk']:
+                print('Sorry, not enough ingredients. Call anyone to add them.')
+            else:
+                pattern = 'The {} costs {} dollars. Would you like to continue? '.format(request, recipes[request]['cost'])
+                answer = input(pattern).lower()
+                if answer == 'yes' or answer == '':
+                    put_money = get_money(coins)
+                    if put_money < recipes[request]['cost']:
+                        print('Sorry, you put not enough money.')
+                    else:
+                        input(f'Have a nice {request}! Press Enter to continue...\n')
+                        machine['water'] -= recipes[request]['water']
+                        machine['coffee'] -= recipes[request]['coffee']
+                        machine['milk'] -= recipes[request]['milk']
+                        machine['money'] += put_money
+                elif answer == 'no':
+                    print('Aborting process...')
+                else:
+                    print('Incorrect answer.')
---
+++ Day_015_Coffee_Machine/coffeeMachine.py
@@ -0,0 +1,14 @@
+﻿recipes = {
+    'espresso': {'water': 30, 'coffee': 10, 'milk': 0, 'cost': 1.5},
+    'americano': {'water': 240, 'coffee': 10, 'milk': 0, 'cost': 2.1},
+    "capucchino": {'water': 100, 'coffee': 10, 'milk': 100, 'cost': 3.4},
+    'latte': {'water': 100, 'coffee': 10, 'milk': 150, 'cost': 4.5}
+}
+
+machine = {
+    'water': 1000, 'coffee': 100, 'milk': 300, 'money': 0
+}
+
+coins = {
+    'Penny': 0.01, 'Nickel': 0.05, 'Dime': 0.1, 'Quarter': 0.25
+}
